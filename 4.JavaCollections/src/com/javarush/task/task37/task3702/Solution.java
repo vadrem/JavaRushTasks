f38884b6e85df0f1e2330866244a8d9a45182bd4
Vadim Rempel <vadrem@gmail.com>
2018-12-24 16:36:25 +0100

    resolved task 3702.2 and out from .gitignort deleted

--- 4.JavaCollections/src/com/javarush/task/task37/task3702/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/Solution.java
@@ -1,9 +1,18 @@
 package com.javarush.task.task37.task3702;
 
+import com.javarush.task.task37.task3702.male.MaleFactory;
+
 public class Solution {
 
     public static void main(String[] args) {
+        MaleFactory mF = new MaleFactory();
+        Human h1 = mF.getPerson(99);
+        Human h2 = mF.getPerson(4);
+        Human h3 = mF.getPerson(15);
 
+        System.out.println(h1);
+        System.out.println(h2);
+        System.out.println(h3);
     }
 
 }
